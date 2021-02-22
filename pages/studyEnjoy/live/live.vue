206c39a76718b8ebabc64f0b5ba419b98c7e355a
Duriamr <duriamr@outlook.com>
2021-02-22 15:47:05 +0800

    live

---
+++ pages/studyEnjoy/live/live.vue
@@ -0,0 +1,78 @@
+<template>
+	<view class="live_contact">
+		<view class="live_ul">
+			<view class="live_list">
+				<view class="list_left">
+					<text class="left_title">锂离子电池设计的基本原理</text>
+					<view class="left_bot">
+						<text>孙安涛</text>
+						<text class="time"></text>
+					</view>
+				</view>
+				<view class="list_right">
+					<view class="img_box">
+						<image class="image" src=""></image>
+					</view>
+					<text>477人报名</text>
+				</view>
+			</view>
+			<view class="live_list">
+				<view class="list_left">
+					<text class="left_title">X射线光电子能谱原理及技术 特点</text>
+					<view class="left_bot">
+						<text>孙安涛</text>
+					</view>
+				</view>
+				<view class="list_right">
+					<view class="img_box">
+						<image class="image" src=""></image>
+					</view>
+					<text>477人报名</text>
+				</view>
+			</view>
+			<view class="live_list">
+				<view class="list_left">
+					<text class="left_title">X射线光电子能谱原理及技术 特点X射线光电子能谱原理及技术 特点X射线光电子能谱原理及技术 特点X射线光电子能谱原理及技术 特点</text>
+					<view class="left_bot">
+						<text>孙安涛</text>
+						<text class="time">2021-01-01 17:00</text>
+					</view>
+				</view>
+				<view class="list_right">
+					<view class="img_box">
+						<image class="image" src=""></image>
+					</view>
+					<text>477人报名</text>
+				</view>
+			</view>
+			<view class="live_list">
+				<view class="list_left">
+					<text class="left_title">X射线光电子能谱原理及技术 特点</text>
+					<view class="left_bot">
+						<text>孙安涛</text>
+					</view>
+				</view>
+				<view class="list_right">
+					<view class="img_box">
+						<image class="image" src=""></image>
+					</view>
+					<text>477人播放</text>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "./live.scss";
+</style>
